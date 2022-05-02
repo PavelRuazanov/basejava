48cb1b0e9411a975a3af5d226605ec840288cc64
Pavel Ryazanov <pavelruazanov@gmail.com>
2022-05-02 13:03:40 +0500

    BaseJava Test

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,51 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        size = 0;
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)){
+                resume = storage[i];
+            }
+        }
+return resume;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                for (int j = i; j < size; j++) {
+                    storage[i] = storage[i + 1];
+                }
+                size--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] array = new Resume[size];
+        System.arraycopy(storage, 0, array, 0, size);
+        return array;
     }
 
-    int size() {
-        return 0;
+    public int size() {
+        return size;
     }
 }
