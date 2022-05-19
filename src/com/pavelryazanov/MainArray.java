fa80ecc7b7bab1fad7a19769dbe46ec2ff9ef1fa
Pavel Ryazanov <pavelruazanov@gmail.com>
2022-05-19 20:37:24 +0500

    BaseJava HW2

--- src/com/pavelryazanov/MainArray.java
+++ src/com/pavelryazanov/MainArray.java
@@ -1,9 +1,13 @@
+package com.pavelryazanov;
+
+import com.pavelryazanov.model.Resume;
+import com.pavelryazanov.storage.ArrayStorage;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Interactive test for ArrayStorage implementation
  * (just run, no need to understand)
  */
 public class MainArray {
@@ -32,7 +36,7 @@ public class MainArray {
                     break;
                 case "save":
                     r = new Resume();
-                    r.uuid = uuid;
+                    r.setUuid(uuid);
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
